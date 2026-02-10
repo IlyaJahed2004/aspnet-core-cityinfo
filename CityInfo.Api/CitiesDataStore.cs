876a645219492573b18176db3d969546645d7a30
ilya jahed <ilya.j.h.d@gmail.com>
2026-02-10 22:41:30 +0330

    feat(PointsOfInterest): add controller with initial get endpoint and nested routing

--- CityInfo.Api/CitiesDataStore.cs
+++ CityInfo.Api/CitiesDataStore.cs
@@ -19,31 +19,85 @@ namespace CityInfo.Api
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
-                    Description = "The one with the cathedral that was never really finished."
+                    Description = "The one with the cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 3,
+                            Name = "Cathedral of Our Lady",
+                            Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 4,
+                            Name = "Antwerp Central Station",
+                            Description = "The finest example of railway architecture in Belgium."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Paris",
-                    Description = "The one with that big tower."
+                    Description = "The one with that big tower.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought-iron lattice tower on the Champ de Mars."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 6,
+                            Name = "The Louvre",
+                            Description = "The world's largest art museum."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 4,
-                    Name = "Tokyo",
-                    Description = "The one with the busy crossing and neon lights."
-                },
-                new CityDto()
-                {
-                    Id = 5,
                     Name = "Tehran",
-                    Description = "The bustling capital with beautiful mountains in the north."
+                    Description = "The bustling capital with beautiful mountains in the north.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 7,
+                            Name = "Milad Tower",
+                            Description = "A multi-purpose tower in Tehran. It is the sixth-tallest tower."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 8,
+                            Name = "Golestan Palace",
+                            Description = "The oldest of the historic monuments in Tehran, a world heritage site."
+                        }
+                    }
                 }
             };
         }
---
+++ CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,38 @@
+﻿using CityInfo.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/cities/{cityId}/PointsOfInterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+        [HttpGet("{PointOfInterestId}")]
+        public ActionResult<PointOfInterestDto> GetSpecificPointOfInterestOfCity(int cityId, int PointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var PointOfInterest = city.PointsOfInterest.FirstOrDefault(points => points.Id == PointOfInterestId);
+            if(PointOfInterest == null)
+            { 
+                return NotFound();
+            }
+            return Ok(PointOfInterest);
+        }
+    }
+}
